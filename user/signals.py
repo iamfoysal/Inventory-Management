38c556339383c6a6b0728c850144372a99c90a3a
foysal <foysalf652@gmail.com>
2022-08-24 22:07:19 +0600

    user profie update

--- user/signals.py
+++ user/signals.py
@@ -3,13 +3,13 @@ from django.template.loader import render_to_string, get_template
 from django.db.models.signals import post_save
 from django.conf import settings
 from django.dispatch import receiver
-from .models import User, Users
+from .models import User, Profile
 
 
 @receiver(post_save, sender=User)
 def create_user(sender, instance, created, **kwargs):
     if created and instance.email:
-        Users.objects.create(user=instance)
+        Profile.objects.create(user=instance)
         
         message = render_to_string('user/accountmail.html', {'name': instance.get_full_name()})
         send_mail(
